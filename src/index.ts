d1e31dd964ba54970a67eb35360a8e4604201da6
muhammad 123 <aliareq4@gmail.com>
2025-05-14 01:13:02 +0300

    запуск сервера

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-  console.log('Happy developing ✨')
+import express from 'express';
+const server = express();
+const port = 2000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
