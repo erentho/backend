9d9fb2cb72b9684680cb3c8898b478e3733b4efd
muhammad 123 <aliareq4@gmail.com>
2025-05-14 17:03:50 +0300

    feat/ добавлен обработчик для POST /user/login

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ const server = express();
 const port = 2000;
 server.post('/user/register', (req, res) => {
   res.json({ message: 'Вы пытаетесь зарегистрироваться!' });
+  server.post('/user/login', (req, res) => {
+    res.json({ message: 'Вы пытаетесь войти!' });
+  });
 });
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
