2647fdcab54140c9ebfdb1c3ac6eefaba39af6e4
muhammad 123 <aliareq4@gmail.com>
2025-05-14 16:42:20 +0300

    добавлен обработчик для POST /user/register

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 const server = express();
 const port = 2000;
+server.post('/user/register', (req, res) => {
+  res.json({ message: 'Вы пытаетесь зарегестрироваться!' });
+});
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
