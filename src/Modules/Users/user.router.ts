0271f76c67341b95416b9ccf754a554e0aa1e31e
muhammad 123 <aliareq4@gmail.com>
2025-05-21 17:38:30 +0300

    feat: добавлен log-routes

--- src/Modules/Users/user.router.ts
+++ src/Modules/Users/user.router.ts
@@ -2,17 +2,17 @@ import express from 'express';
 import logger from '../../pino.logger';
 export const userRouter = express.Router();
 
-userRouter.post('/user/register', (req, res) => {
+userRouter.post('/register', (req, res) => {
   logger.info ("Registration")
-  res.json({ message: 'Вы пытаетесь зарегистрироваться!' });
+ res.json({ message: 'Вы пытаетесь зарегистрироваться!' });
 });
 
-userRouter.post('/user/login', (req, res) => {
+userRouter.post('/login', (req, res) => {
   logger.info ("Login")
   res.json({ message: 'Вы пытаетесь войти!' });
 });
 
-userRouter.post('/user/logout', (req, res) => {
+userRouter.post('/logout', (req, res) => {
   logger.info ("Logout")
   res.json({ message: 'Вы пытаетесь выйти!' });
 });
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import logger from './pino.logger';
 import { userRouter } from './Modules/Users/user.router';
 import { taskRouter } from './Modules/Tasks/tasks.router';
+import { logRoutes } from './Bootstrap/log-routes';
 
 const server = express();
 const port = 2000;
 server.use('/task', taskRouter);
 server.use('/user', userRouter);
 
-
+logRoutes(server);
 
 server.listen(port, () => {
   logger.info ("Listening on port 2000")
