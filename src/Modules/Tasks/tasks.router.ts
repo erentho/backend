bb6eb8cb073c651fd010d609615e8eaf0ff03b2e
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-05-23 19:04:09 +0300

    fix: Исправлен tasks.router.ts

--- src/Modules/Tasks/tasks.router.ts
+++ src/Modules/Tasks/tasks.router.ts
@@ -1,5 +1,5 @@
 import logger from '../../pino.logger';
-import express from 'express';
+import express, { Request, Response } from 'express';
 export const taskRouter = express.Router();
 
 taskRouter.get('/:id', (req, res) => {
@@ -7,11 +7,13 @@ taskRouter.get('/:id', (req, res) => {
   logger.info ("Task")
   res.json({ message: `Вы пытаетесь прочитать задачу по id= ${taskId}` });
 });
+
 taskRouter.put('/:id', (req: Request, res: Response) => {
   const taskId = req.params.id;
   logger.info ("Task updated")
   res.json({ message: `Вы пытаетесь обновить задачу по id=${taskId}` });
 });
+
 taskRouter.put('/task', (req: Request, res: Response) => {
   logger.info ("[]")
   res.json([]);
