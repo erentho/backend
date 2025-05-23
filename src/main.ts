e7728617a26696c6efc438a1b22945ff533e422b
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-05-23 19:08:29 +0300

    fix: Исправлен ESLint и TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import express from 'express';
+import { logRoutes } from './bootstrap/log-routes';
+import { taskRouter } from './modules/tasks/tasks.router';
+import { userRouter } from './modules/users/user.router';
 import logger from './pino.logger';
-import { userRouter } from './Modules/Users/user.router';
-import { taskRouter } from './Modules/Tasks/tasks.router';
-import { logRoutes } from './Bootstrap/log-routes';
 
 const server = express();
 const port = 2000;
@@ -12,6 +12,6 @@ server.use('/user', userRouter);
 logRoutes(server);
 
 server.listen(port, () => {
-  logger.info ("Listening on port 2000")
+  logger.info('Listening on port 2000');
   console.log(`Listening on port ${port}`);
 });
